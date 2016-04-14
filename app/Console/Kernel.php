15de540e3e37a2d4ec84a5390dddfcd4f4de1f3b
ell <ell@cybercog.su>
2016-04-14 04:56:56 +0300

    Fix destroy outdated records in schedule.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('secu:clean-outdated')->everyMinute();
+        $schedule->command('secu:destroy-outdated')->everyMinute();
     }
 }
